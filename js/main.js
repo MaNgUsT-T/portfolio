bec07e1daec0c8ad11b7412658ae8735c4467b3c
a.lehn <a.lehn@clinton.de>
2026-05-13 15:08:35 +0200

    header footer layout

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+ * Icons
+*/
+
+lucide.createIcons();
+
+
+/*
+ * Header scrolled
+*/
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 88) {
+        header.classList.add('header--scrolled');
+    } else {
+        header.classList.remove('header--scrolled');
+    }
+});

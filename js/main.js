85e8451b888ca41b8805f2e4126abbd8a63dbc78
a.lehn <a.lehn@clinton.de>
2026-05-28 10:01:37 +0200

    education carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*
- * Header scrolled
+* Header scrolled
 */
 
 window.addEventListener('scroll', function() {
@@ -12,23 +12,68 @@ window.addEventListener('scroll', function() {
 });
 
 /*
- * Education carousel
+* Education carousel
 */
 
+let educationCarouselItems = $('#education-carousel').find('.splide__slide').length;
+console.log(educationCarouselItems);
+
+/*
+* JS
+*/
+/*
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#education-carousel', {
-        type   : 'loop',
+        type: 'loop',
         perPage: 1,
         perMove: 1,
         gap: '2.4rem',
+        destroy: 1 >= educationCarouselItems ? true : false,
         mediaQuery: 'min',
         breakpoints: {
             768: {
                 perPage: 2,
+                perMove: 2,
+                destroy: 2 >= educationCarouselItems ? true : false
             },
             1024: {
                 perPage: 4,
-            },
+                perMove: 4,
+                destroy: 4 >= educationCarouselItems ? true : false
+            }
         }
     } ).mount();
 } );
+*/
+
+/*
+* JQuery
+*/
+function educationCarouselInitialize() {
+    if (educationCarouselItems > 0) {
+        const educationItemList = new Splide( '#education-carousel', {
+            type: 'loop',
+            perPage: 1,
+            perMove: 1,
+            gap: '2.4rem',
+            destroy: 1 >= educationCarouselItems ? true : false,
+            mediaQuery: 'min',
+            breakpoints: {
+                768: {
+                    perPage: 2,
+                    perMove: 2,
+                    destroy: 2 >= educationCarouselItems ? true : false
+                },
+                1024: {
+                    perPage: 4,
+                    perMove: 4,
+                    destroy: 4 >= educationCarouselItems ? true : false
+                }
+            }
+        });
+        educationItemList.mount();
+    }
+}
+(function($) {
+    $(document).ready(educationCarouselInitialize);
+})(jQuery);

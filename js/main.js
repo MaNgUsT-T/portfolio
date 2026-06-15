78f5908fa9be1e8fb000d65128bdd593764136dd
a.lehn <a.lehn@clinton.de>
2026-06-15 15:01:04 +0200

    splide slider

--- js/main.js
+++ js/main.js
@@ -11,11 +11,42 @@ window.addEventListener('scroll', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var splide = new Splide('.splide', {
+
+
+/*
+ * Carousel
+ *
+*/
+
+let educationCarouselItems = document.querySelectorAll('#education-carousel .splide__slide').length;
+console.log('Education Items:', educationCarouselItems);
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#education-carousel', {
         type: 'loop',
+        perPage: 1,
+        perMove: 1,
+        gap: '2.4rem',
+        destroy: 1 >= educationCarouselItems ? true : false,
+        mediaQuery: 'min',
+        breakpoints: {
+            768: {
+                perPage: 2,
+                perMove: 2,
+                destroy: 2 >= educationCarouselItems ? true : false
+            },
+            1024: {
+                perPage: 4,
+                perMove: 4,
+                destroy: 4 >= educationCarouselItems ? true : false
+            }
+        }
+    } ).mount();
+} );
 
 
-    });
-    splide.mount();
-});
+
+
+
+
+
